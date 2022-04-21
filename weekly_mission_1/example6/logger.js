ea18bd090b012a3f09436ee3843099602dec5957
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-04-21 00:33:47 -0500

    add: logger.js - Modificación de clases

---
+++ weekly_mission_1/example6/logger.js
@@ -0,0 +1,16 @@
+// Example 6: Modificación de clases
+
+class Logger {
+    constructor(name) {
+        this.count = 0
+        this.name = name
+    }
+
+    log(message) {
+        this.count++
+        console.log('[' + this.name + '] ' + message)
+    }
+}
+
+module.exports = new Logger('DEFAULT') // Nuevo objeto instanciado
+module.exports.Logger = Logger // Clase
