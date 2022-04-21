af36b2460b4f3215bd770a37bec3be069d951de6
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-04-21 00:21:22 -0500

    add: logger.js - Clases y Objetos

---
+++ weekly_mission_1/example5/logger.js
@@ -0,0 +1,16 @@
+// Example 5: Clases y Objetos
+
+class Logger {
+    constructor(name) {
+        // Al crear este objeto se guardarán estos valores
+        this.count = 0
+        this.name = name
+    }
+
+    log(message) {
+        this.count++ // Se aumenta el contador interno al llamar este método
+        console.log('[' + this.name + '] ' + message)
+    }
+}
+
+module.exports = new Logger('DEFAULT') // Instanciación del objeto y se exporta
