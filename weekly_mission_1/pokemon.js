29f78aa52184f406d49d188339df83d9a30d55b9
Mauricio Reyes <mauricioreyes.dev@gmail.com>
2022-04-21 22:12:55 -0500

    add: main.js & pokemon.js - Ejercicio 1

---
+++ weekly_mission_1/pokemon.js
@@ -0,0 +1,17 @@
+// Example 9: Ejercicio
+
+class Pokemon {
+    constructor(name) {
+        this.name = name
+    }
+
+    sayHello () {
+        console.log(`Mi pokemon ${this.name} te saluda!!!`)
+    }
+
+    sayMessage (message) {
+        console.log(`Mi pokemon ${this.name} dice: ${message}`)
+    }
+}
+
+module.exports = Pokemon
